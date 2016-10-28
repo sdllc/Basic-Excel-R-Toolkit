c661f2ebd79dd4a947a2701c2e6519a06427eb05
Duncan Werner <dwerner@riskamp.com>
2016-10-28 11:00:05 -0700

    slight refactor

--- BERT/RInterface.cpp
+++ BERT/RInterface.cpp
@@ -1388,8 +1388,22 @@ void SEXP2XLOPER(LPXLOPER12 xloper, SEXP sexp, bool inner = false, int r_offset 
 		XLOPER12 xlrslt;
 		Excel12(xlfCaller, &xlrslt, 0, 0);
 		if (xlrslt.xltype == xltypeSRef) {
+
 			xlrows = xlrslt.val.sref.ref.rwLast - xlrslt.val.sref.ref.rwFirst + 1;
 			xlcols = xlrslt.val.sref.ref.colLast - xlrslt.val.sref.ref.colFirst + 1;
+
+			xllen = xlrows * xlcols;
+			if (xllen > 1) {
+				xloper->xltype = xltypeMulti | xlbitDLLFree;
+				xloper->val.array.rows = xlrows;
+				xloper->val.array.columns = xlcols;
+				xloper->val.array.lparray = new XLOPER12[xlrows * xlcols];
+				for (int i = 0; i < xllen; i++) {
+					xloper->val.array.lparray[i].xltype = xltypeStr; // no free bit: this is static
+					xloper->val.array.lparray[i].val.str = emptyStr;
+				}
+			}
+
 		}
 		else if (xlrslt.xltype == xltypeErr && xlrslt.val.err == xlerrRef ) {
 
@@ -1404,19 +1418,6 @@ void SEXP2XLOPER(LPXLOPER12 xloper, SEXP sexp, bool inner = false, int r_offset 
 		}
 		Excel12(xlFree, 0, 1, (LPXLOPER12)&xlrslt);
 
-		if (!api_call) {
-			xllen = xlrows * xlcols;
-			if (xllen > 1) {
-				xloper->xltype = xltypeMulti | xlbitDLLFree;
-				xloper->val.array.rows = xlrows;
-				xloper->val.array.columns = xlcols;
-				xloper->val.array.lparray = new XLOPER12[xlrows * xlcols];
-				for (int i = 0; i < xllen; i++) {
-					xloper->val.array.lparray[i].xltype = xltypeStr; // no free bit: this is static
-					xloper->val.array.lparray[i].val.str = emptyStr;
-				}
-			}
-		}
 	}
 
 	// always do this (unless it's an array)
