145358667b22314af4b9d8db96636f49a4041fc5
Duncan Werner <dwerner@riskamp.com>
2017-03-13 05:28:20 -0700

    update for R version upgrade

--- BERT/StringConstants.h
+++ BERT/StringConstants.h
@@ -35,8 +35,8 @@
 #define BERT_LINK			L"http://" BERT_LINK_TEXT
 
 #define ABOUT_R_TEXT \
-	L"Includes R version 3.3.2 (2016-10-31) -- \"Sincere Pumpkin Patch\"\r\n" \
-	L"Copyright (C) 2016 The R Foundation for Statistical Computing"
+	L"Includes R version 3.3.3 (2017-03-06) -- \"Another Canoe\"\r\n" \
+	L"Copyright (C) 2017 The R Foundation for Statistical Computing"
 
 #define R_LINK_TEXT			L"www.r-project.org"
 #define R_LINK				L"http://" R_LINK_TEXT
