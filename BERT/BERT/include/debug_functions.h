3c3b403809357ecd036e03130cff4270e8137cf6
Duncan Werner <dwerner@riskamp.com>
2018-01-24 08:29:51 -0800

    update debug functions

---
+++ BERT/BERT/include/debug_functions.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+
+#ifdef _DEBUG
+
+int DebugOut(const char *fmt, ...);
+
+// thanks to 
+// https://stackoverflow.com/questions/16703835/visual-studio-how-can-i-see-cout-output-in-a-non-console-application
+//
+// although using streams may be preferable from a development standpoint, there's a drawback
+// in that if we start using cout/cerr for development logging, these won't go away in release
+// builds. the benefit of using the old DebugOut function is that it can get completely removed 
+// by the preprocessor. 
+//
+// TODO: stream wrappers that can get scrubbed?
+//
+class dbg_stream_for_stdio : public std::stringbuf
+{
+public:
+  dbg_stream_for_stdio(const std::string &prefix = "") {}
+  ~dbg_stream_for_stdio() { sync(); }
+  int sync()
+  {
+    ::OutputDebugStringA(str().c_str());
+    str(std::string()); 
+    return 0;
+  }
+public:
+  static void InitStreams();
+};
+
+#else 
+#define DebugOut(){}
+#endif
+
--- BERT/BERT/src/debug_functions.cc
+++ BERT/BERT/src/debug_functions.cc
@@ -15,5 +15,13 @@ int DebugOut(const char *fmt, ...)
 	return ret;
 }
 
+dbg_stream_for_stdio g_DebugStreamFor_cout;
+dbg_stream_for_stdio g_DebugStreamFor_cerr;
+
+void dbg_stream_for_stdio::InitStreams() {
+  std::cout.rdbuf(&g_DebugStreamFor_cout);
+  std::cerr.rdbuf(&g_DebugStreamFor_cerr);
+}
+
 #endif
 
