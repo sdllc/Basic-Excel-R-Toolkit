5704d9af68247bfebb01f311d61aeb9fa0a06111
Duncan Werner <dwerner@riskamp.com>
2018-02-14 07:57:07 -0800

    clean up formatting

--- Ribbon/ribbon_connect.cc
+++ Ribbon/ribbon_connect.cc
@@ -6,114 +6,114 @@
 extern CBERT2RibbonModule _AtlModule;
 
 bool GetRegistryString(std::string &result_value, HKEY base_key, const char *key, const char *name) {
-    bool succeeded = false;
-    char buffer[MAX_PATH];
-    DWORD data_size = MAX_PATH;
-    LSTATUS status = RegGetValueA(base_key, key, name, RRF_RT_REG_SZ | RRF_RT_REG_EXPAND_SZ, 0, buffer, &data_size);
-    if (!status && data_size > 0) result_value.assign(buffer, data_size - 1);
-    return (status == 0);
+  bool succeeded = false;
+  char buffer[MAX_PATH];
+  DWORD data_size = MAX_PATH;
+  LSTATUS status = RegGetValueA(base_key, key, name, RRF_RT_REG_SZ | RRF_RT_REG_EXPAND_SZ, 0, buffer, &data_size);
+  if (!status && data_size > 0) result_value.assign(buffer, data_size - 1);
+  return (status == 0);
 }
 
 STDMETHODIMP CConnect::GetCustomUI(BSTR RibbonID, BSTR *pbstrRibbonXML)
 {
-	HRSRC hRsrc = ::FindResource(_AtlBaseModule.m_hInstResource, MAKEINTRESOURCE(IDR_XML2), L"XML");
-	if (hRsrc)
-	{
-		DWORD dwLen = SizeofResource(_AtlBaseModule.m_hInstResource, hRsrc);
-		if (dwLen > 0)
-		{
-			HGLOBAL hGbl = LoadResource(_AtlBaseModule.m_hInstResource, hRsrc);
-			if (hGbl)
-			{
-				LPVOID pData = LockResource(hGbl);
-				if (pData)
-				{
-					CComBSTR bstrData(dwLen, (char*)pData);
-					UnlockResource(hGbl);
-					*pbstrRibbonXML = bstrData.Detach();
-				}
-			}
-		}
-	}
-	return S_OK;
+  HRSRC hRsrc = ::FindResource(_AtlBaseModule.m_hInstResource, MAKEINTRESOURCE(IDR_XML2), L"XML");
+  if (hRsrc)
+  {
+    DWORD dwLen = SizeofResource(_AtlBaseModule.m_hInstResource, hRsrc);
+    if (dwLen > 0)
+    {
+      HGLOBAL hGbl = LoadResource(_AtlBaseModule.m_hInstResource, hRsrc);
+      if (hGbl)
+      {
+        LPVOID pData = LockResource(hGbl);
+        if (pData)
+        {
+          CComBSTR bstrData(dwLen, (char*)pData);
+          UnlockResource(hGbl);
+          *pbstrRibbonXML = bstrData.Detach();
+        }
+      }
+    }
+  }
+  return S_OK;
 }
 
 // CConnect
 STDMETHODIMP CConnect::OnConnection(IDispatch *pApplication, AddInDesignerObjects::ext_ConnectMode /*ConnectMode*/, IDispatch *pAddInInst, SAFEARRAY ** /*custom*/)
 {
-    const char path_separator[] = "\\";
+  const char path_separator[] = "\\";
 
-    std::string xll_path;
-    GetRegistryString(xll_path, HKEY_CURRENT_USER, "Software\\BERT", "BERT2.BinDir");
+  std::string xll_path;
+  GetRegistryString(xll_path, HKEY_CURRENT_USER, "Software\\BERT", "BERT2.BinDir");
 
-    if (xll_path.length() && xll_path.compare(xll_path.length() - 1, 1, path_separator)) {
-        xll_path.append(path_separator);
-    }
+  if (xll_path.length() && xll_path.compare(xll_path.length() - 1, 1, path_separator)) {
+    xll_path.append(path_separator);
+  }
 
 #ifdef _DEBUG 
 #ifdef _WIN64
-    xll_path.append("BERT64D.xll");
+  xll_path.append("BERT64D.xll");
 #else
-    xll_path.append("BERT32D.xll");
+  xll_path.append("BERT32D.xll");
 #endif
 #else
 #ifdef _WIN64
-    xll_path.append("BERT64.xll");
+  xll_path.append("BERT64.xll");
 #else
-    xll_path.append("BERT32.xll");
+  xll_path.append("BERT32.xll");
 #endif
 #endif
-    
-	pApplication->QueryInterface(__uuidof(IDispatch), (LPVOID*)&m_pApplication);
-	pAddInInst->QueryInterface(__uuidof(IDispatch), (LPVOID*)&m_pAddInInstance);
 
-	CComQIPtr<Excel::_Application> app(m_pApplication);
-	if (app){
+  pApplication->QueryInterface(__uuidof(IDispatch), (LPVOID*)&m_pApplication);
+  pAddInInst->QueryInterface(__uuidof(IDispatch), (LPVOID*)&m_pAddInInstance);
 
-        int result = SetPointers(reinterpret_cast<ULONG_PTR>(m_pApplication.p), reinterpret_cast<ULONG_PTR>(this));
-        if( result ){
+  CComQIPtr<Excel::_Application> app(m_pApplication);
+  if (app) {
 
-			_bstr_t bstrPath(xll_path.c_str());
-			VARIANT_BOOL vb = VARIANT_FALSE;
-			HRESULT hr = app->RegisterXLL(bstrPath, 1033, &vb);
+    int result = SetPointers(reinterpret_cast<ULONG_PTR>(m_pApplication.p), reinterpret_cast<ULONG_PTR>(this));
+    if (result) {
 
-			if (SUCCEEDED(hr)){
-				if (vb) {
-                    result = SetPointers(reinterpret_cast<ULONG_PTR>(m_pApplication.p), reinterpret_cast<ULONG_PTR>(this));
-                    // ATLTRACE("register loaded xll OK");
-				}
-				else {
-					ATLTRACE("register call succeeded but load returned false\n");
-				}
-			}
-			else {
-				ATLTRACE("register failed with 0x%x\n", hr);
-			}
-		}
-	}
-    
-	return S_OK;
+      _bstr_t bstrPath(xll_path.c_str());
+      VARIANT_BOOL vb = VARIANT_FALSE;
+      HRESULT hr = app->RegisterXLL(bstrPath, 1033, &vb);
+
+      if (SUCCEEDED(hr)) {
+        if (vb) {
+          result = SetPointers(reinterpret_cast<ULONG_PTR>(m_pApplication.p), reinterpret_cast<ULONG_PTR>(this));
+          // ATLTRACE("register loaded xll OK");
+        }
+        else {
+          ATLTRACE("register call succeeded but load returned false\n");
+        }
+      }
+      else {
+        ATLTRACE("register failed with 0x%x\n", hr);
+      }
+    }
+  }
+
+  return S_OK;
 }
 
 STDMETHODIMP CConnect::OnDisconnection(AddInDesignerObjects::ext_DisconnectMode /*RemoveMode*/, SAFEARRAY ** /*custom*/)
 {
-	m_pApplication = NULL;
-	m_pAddInInstance = NULL;
-	if (m_pRibbonUI) m_pRibbonUI.Release();
-	return S_OK;
+  m_pApplication = NULL;
+  m_pAddInInstance = NULL;
+  if (m_pRibbonUI) m_pRibbonUI.Release();
+  return S_OK;
 }
 
 STDMETHODIMP CConnect::OnAddInsUpdate(SAFEARRAY ** /*custom*/)
 {
-	return S_OK;
+  return S_OK;
 }
 
 STDMETHODIMP CConnect::OnStartupComplete(SAFEARRAY ** /*custom*/)
 {
-	return S_OK;
+  return S_OK;
 }
 
 STDMETHODIMP CConnect::OnBeginShutdown(SAFEARRAY ** /*custom*/)
 {
-	return S_OK;
+  return S_OK;
 }
--- Ribbon/ribbon_connect.h
+++ Ribbon/ribbon_connect.h
@@ -6,148 +6,148 @@
 #include <atlsafe.h>
 
 typedef IDispatchImpl<IRibbonExtensibility, &__uuidof(IRibbonExtensibility), &LIBID_Office, /* wMajor = */ 2, /* wMinor = */ 4> RIBBON_INTERFACE;
-typedef int (*SetPointersProcedure)(ULONG_PTR, ULONG_PTR);
+typedef int(*SetPointersProcedure)(ULONG_PTR, ULONG_PTR);
 
 typedef enum {
-    ShowConsole = 1001
+  ShowConsole = 1001
 } DispIds;
 
 // CConnect
 class ATL_NO_VTABLE CConnect :
-	public CComObjectRootEx<CComSingleThreadModel>,
-	public CComCoClass<CConnect, &CLSID_Connect>,
-	public IDispatchImpl<AddInDesignerObjects::_IDTExtensibility2, &AddInDesignerObjects::IID__IDTExtensibility2, &AddInDesignerObjects::LIBID_AddInDesignerObjects, 1, 0>,
-	public RIBBON_INTERFACE
+  public CComObjectRootEx<CComSingleThreadModel>,
+  public CComCoClass<CConnect, &CLSID_Connect>,
+  public IDispatchImpl<AddInDesignerObjects::_IDTExtensibility2, &AddInDesignerObjects::IID__IDTExtensibility2, &AddInDesignerObjects::LIBID_AddInDesignerObjects, 1, 0>,
+  public RIBBON_INTERFACE
 {
 public:
-	CConnect() : m_pApplication(0)
-	{
-	}
+  CConnect() : m_pApplication(0)
+  {
+  }
 
-	DECLARE_REGISTRY_RESOURCEID(IDR_BERTRIBBON2)
-	DECLARE_NOT_AGGREGATABLE(CConnect)
+  DECLARE_REGISTRY_RESOURCEID(IDR_BERTRIBBON2)
+  DECLARE_NOT_AGGREGATABLE(CConnect)
 
-	BEGIN_COM_MAP(CConnect)
-		COM_INTERFACE_ENTRY2(IDispatch, IRibbonExtensibility)
-		COM_INTERFACE_ENTRY(AddInDesignerObjects::_IDTExtensibility2)
-		COM_INTERFACE_ENTRY(IRibbonExtensibility)
-	END_COM_MAP()
+  BEGIN_COM_MAP(CConnect)
+    COM_INTERFACE_ENTRY2(IDispatch, IRibbonExtensibility)
+    COM_INTERFACE_ENTRY(AddInDesignerObjects::_IDTExtensibility2)
+    COM_INTERFACE_ENTRY(IRibbonExtensibility)
+  END_COM_MAP()
 
-	DECLARE_PROTECT_FINAL_CONSTRUCT()
+  DECLARE_PROTECT_FINAL_CONSTRUCT()
 
-	HRESULT FinalConstruct()
-	{
-		return S_OK;
-	}
+  HRESULT FinalConstruct()
+  {
+    return S_OK;
+  }
 
-	void FinalRelease()
-	{
-	}
-
-public:
+  void FinalRelease()
+  {
+  }
 
 public:
-	//IDTExtensibility2 implementation:
-	STDMETHOD(OnConnection)(IDispatch * Application, AddInDesignerObjects::ext_ConnectMode ConnectMode, IDispatch *AddInInst, SAFEARRAY **custom);
-	STDMETHOD(OnDisconnection)(AddInDesignerObjects::ext_DisconnectMode RemoveMode, SAFEARRAY **custom);
-	STDMETHOD(OnAddInsUpdate)(SAFEARRAY **custom);
-	STDMETHOD(OnStartupComplete)(SAFEARRAY **custom);
-	STDMETHOD(OnBeginShutdown)(SAFEARRAY **custom);
 
-	CComPtr<IDispatch>      m_pApplication;
-	CComPtr<IDispatch>      m_pAddInInstance;
-	CComQIPtr<IRibbonUI>    m_pRibbonUI;
+public:
+  //IDTExtensibility2 implementation:
+  STDMETHOD(OnConnection)(IDispatch * Application, AddInDesignerObjects::ext_ConnectMode ConnectMode, IDispatch *AddInInst, SAFEARRAY **custom);
+  STDMETHOD(OnDisconnection)(AddInDesignerObjects::ext_DisconnectMode RemoveMode, SAFEARRAY **custom);
+  STDMETHOD(OnAddInsUpdate)(SAFEARRAY **custom);
+  STDMETHOD(OnStartupComplete)(SAFEARRAY **custom);
+  STDMETHOD(OnBeginShutdown)(SAFEARRAY **custom);
 
-	// IRibbonExtensibility methods
-	STDMETHOD(GetCustomUI)(BSTR RibbonID, BSTR *pbstrRibbonXML);
+  CComPtr<IDispatch>      m_pApplication;
+  CComPtr<IDispatch>      m_pAddInInstance;
+  CComQIPtr<IRibbonUI>    m_pRibbonUI;
 
-	// IDispatch methods
-	STDMETHOD(GetIDsOfNames)(REFIID riid, LPOLESTR* rgszNames, UINT cNames, LCID lcid, DISPID* rgdispid) {
+  // IRibbonExtensibility methods
+  STDMETHOD(GetCustomUI)(BSTR RibbonID, BSTR *pbstrRibbonXML);
 
-        int disp_id = 0;
+  // IDispatch methods
+  STDMETHOD(GetIDsOfNames)(REFIID riid, LPOLESTR* rgszNames, UINT cNames, LCID lcid, DISPID* rgdispid) {
 
-        if (cNames > 0) {
-            if (!wcscmp(rgszNames[0], L"ShowConsole")) disp_id = DispIds::ShowConsole;
-        }
+    int disp_id = 0;
 
-		if (disp_id > 0)
-		{
-			rgdispid[0] = disp_id;
-			return S_OK;
-		}
+    if (cNames > 0) {
+      if (!wcscmp(rgszNames[0], L"ShowConsole")) disp_id = DispIds::ShowConsole;
+    }
 
-		return RIBBON_INTERFACE::GetIDsOfNames(riid, rgszNames, cNames, lcid, rgdispid);
-	}
+    if (disp_id > 0)
+    {
+      rgdispid[0] = disp_id;
+      return S_OK;
+    }
 
-	STDMETHOD(Invoke)(DISPID dispidMember, REFIID riid,
-		LCID lcid, WORD wFlags, DISPPARAMS* pdispparams, VARIANT* pvarResult,
-		EXCEPINFO* pexcepinfo, UINT* puArgErr)
-	{
-		static CComBSTR bstr;
-        switch (dispidMember)
+    return RIBBON_INTERFACE::GetIDsOfNames(riid, rgszNames, cNames, lcid, rgdispid);
+  }
+
+  STDMETHOD(Invoke)(DISPID dispidMember, REFIID riid,
+    LCID lcid, WORD wFlags, DISPPARAMS* pdispparams, VARIANT* pvarResult,
+    EXCEPINFO* pexcepinfo, UINT* puArgErr)
+  {
+    static CComBSTR bstr;
+    switch (dispidMember)
+    {
+    case DispIds::ShowConsole:
+      return HandleControlInvocation("BERT.Console()");
+    }
+
+    return RIBBON_INTERFACE::Invoke(dispidMember, riid, lcid, wFlags, pdispparams, pvarResult, pexcepinfo, puArgErr);
+  }
+
+  // Local Methods
+
+  int SetPointers(ULONG_PTR excel_pointer, ULONG_PTR ribbon_pointer)
+  {
+    HANDLE handle = GetCurrentProcess();
+    HMODULE module_handles[1024];
+    DWORD bytes;
+    char buffer[MAX_PATH];
+
+    if (EnumProcessModules(handle, module_handles, sizeof(module_handles), &bytes))
+    {
+      int count = bytes / sizeof(HANDLE);
+      for (int i = 0; i < count; i++)
+      {
+        if (GetModuleFileNameExA(handle, module_handles[i], buffer, MAX_PATH))
         {
-        case DispIds::ShowConsole:
-            return HandleControlInvocation("BERT.Console()");
+          std::string module_name(buffer);
+          if (std::string::npos != module_name.find("BERT") && std::string::npos != module_name.find(".xll"))
+          {
+            auto procedure = GetProcAddress(module_handles[i], "BERT_SetPointers");
+            if (procedure) {
+              SetPointersProcedure set_pointers = reinterpret_cast<SetPointersProcedure>(procedure);
+              set_pointers(excel_pointer, ribbon_pointer);
+              return 0;
+            }
+          }
         }
+      }
+    }
 
-		return RIBBON_INTERFACE::Invoke(dispidMember, riid, lcid, wFlags, pdispparams, pvarResult, pexcepinfo, puArgErr);
-	}
+    return -1;
+  }
 
-	// Local Methods
+  HRESULT HandleCacheRibbon(DISPPARAMS* pdispparams)
+  {
+    if (pdispparams->cArgs > 0
+      && pdispparams->rgvarg[0].vt == VT_DISPATCH)
+    {
+      m_pRibbonUI = pdispparams->rgvarg[0].pdispVal;
+      return S_OK;
+    }
+    return E_FAIL;
+  }
 
-	int SetPointers(ULONG_PTR excel_pointer, ULONG_PTR ribbon_pointer)
-	{
-		HANDLE handle = GetCurrentProcess();
-		HMODULE module_handles[1024];
-		DWORD bytes;
-        char buffer[MAX_PATH];
-
-		if (EnumProcessModules(handle, module_handles, sizeof(module_handles), &bytes))
-		{
-            int count = bytes / sizeof(HANDLE);
-			for (int i = 0; i < count; i++)
-			{
-				if (GetModuleFileNameExA(handle, module_handles[i], buffer, MAX_PATH))
-				{
-					std::string module_name(buffer);
-					if (std::string::npos != module_name.find("BERT") && std::string::npos != module_name.find(".xll"))
-					{
-                        auto procedure = GetProcAddress(module_handles[i], "BERT_SetPointers");
-                        if( procedure ){
-                            SetPointersProcedure set_pointers = reinterpret_cast<SetPointersProcedure>(procedure);
-                            set_pointers(excel_pointer, ribbon_pointer);
-                            return 0;
-						}
-					}
-				}
-			}
-		}
-
-		return -1;
-	}
-    
-	HRESULT HandleCacheRibbon(DISPPARAMS* pdispparams)
-	{
-		if (pdispparams->cArgs > 0
-			&& pdispparams->rgvarg[0].vt == VT_DISPATCH)
-		{
-			m_pRibbonUI = pdispparams->rgvarg[0].pdispVal;
-			return S_OK;
-		}
-		return E_FAIL;
-	}
-
-	HRESULT HandleControlInvocation(const char *szCmd)
-	{
-		CComQIPtr< Excel::_Application > pApp(m_pApplication);
-		if (pApp)
-		{
-			CComBSTR bstrCmd(szCmd);
-			CComVariant cvRslt;
-			HRESULT hr = pApp->ExecuteExcel4Macro(bstrCmd, 1033, &cvRslt);
-		}
-		return S_OK;
-	}
+  HRESULT HandleControlInvocation(const char *szCmd)
+  {
+    CComQIPtr< Excel::_Application > pApp(m_pApplication);
+    if (pApp)
+    {
+      CComBSTR bstrCmd(szCmd);
+      CComVariant cvRslt;
+      HRESULT hr = pApp->ExecuteExcel4Macro(bstrCmd, 1033, &cvRslt);
+    }
+    return S_OK;
+  }
 
 };
 
