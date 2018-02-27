7f42f46fe9e46336b3d7b9f7ab85ffc9ab1c76c8
Duncan Werner <dwerner@riskamp.com>
2018-02-27 09:32:25 -0800

    clean up

--- Examples/excel-scripting.jl
+++ Examples/excel-scripting.jl
@@ -13,30 +13,47 @@ and execute).
 
 =#
 
+#
 # get the active workbook
+#
 wb = EXCEL.Application.get_ActiveWorkbook()
 
+#
 # get the worksheets pointer and add a new sheet
+#
 new_sheet = wb.get_Worksheets().Add()
 
+#
 # change the name. note this will fail if a sheet with this name 
 # already exists (like if you run this code twice)
+#
 new_sheet.put_Name("Julia Data")
 
+#
 # create some test data -- here it's a 20x5 array
+#
 data = reshape(randn(100), (20,5))
 
+#
 # now insert this into the new sheet
+#
 new_sheet.get_Range("B2:F21").put_Value(data)
 
-# add some headers. NOTE: transponse does not work on strings in 
-# julia? seems like an oversight. this is clumsy.
+#
+# add some headers. NOTE: transponse does not work on 
+# strings in julia? seems like an oversight. 
+#
 header_range = new_sheet.get_Range("B1:F1")
-header_range.put_Value(reshape(["A", "B", "C", "D", "E"], (1,5)))
+header_range.put_Value(hcat("A", "B", "C", "D", "E"))
 
+#
 # example of using constants: add border (underline)
+#
 border = header_range.get_Borders(EXCEL.XlBordersIndex.xlEdgeBottom)
 border.put_Weight( EXCEL.XlBorderWeight.xlThin )
 
+#
 # center headers
+#
 header_range.put_HorizontalAlignment(EXCEL.Constants.xlCenter)
+
