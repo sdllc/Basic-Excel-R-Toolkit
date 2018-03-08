18f42c61a7b7d0960cd7ecd75cf1190d18709164
Duncan Werner <dwerner@riskamp.com>
2018-03-08 14:22:52 -0800

    add banners

--- Console/src/shell/terminal_implementation.ts
+++ Console/src/shell/terminal_implementation.ts
@@ -1,3 +1,21 @@
+/**
+ * Copyright (c) 2017-2018 Structured Data, LLC
+ * 
+ * This file is part of BERT.
+ *
+ * BERT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import { Terminal as XTerm, ITerminalOptions, ITheme as ITerminalTheme } from 'xterm';
 import { Base64 } from 'js-base64';
--- Console/src/ui/user_stylesheet.ts
+++ Console/src/ui/user_stylesheet.ts
@@ -1,13 +1,26 @@
+/**
+ * Copyright (c) 2017-2018 Structured Data, LLC
+ * 
+ * This file is part of BERT.
+ *
+ * BERT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import * as less from 'less';
 import * as path from 'path';
 import * as fs from 'fs';
 import { FileWatcher } from '../common/file_watcher'; 
-
-// this is a ts bug, will be fixed in 2.8  
-
-//fs; 
-//less;
 
 export class UserStylesheet {
 
